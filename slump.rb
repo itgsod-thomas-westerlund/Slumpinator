dddcc53bc8bd519b2378a035810eaab296438088
itgsod-thomas-westerlund <thomas.westerlund@elev.it-gymnasiet.se>
2015-05-05 16:20:06 +0200

    presence def
    presence moved to own def

--- slump.rb
+++ slump.rb
@@ -4,12 +4,15 @@ def student_list_maker
 
   # Public:
   #
-  # <How it works/Example>
+  # How it works/Example:
   # elev_list_maker
   # students.txt = Daniel_Berg 9
   #                Bosse_Python 8
   #
-  # => list_of_all_students = [#<Student:0x007fe2ca020968>, #<Student:0x007fe2ca020238>]
+  # => list_of_all_students = [Daniel, Bosse]
+  #
+  #  Daniel = #<Student:0x007fe2ca020968> => name: Daneil_Berg, grade: 9, opt_grade_min: 6, opt_grade_max: 12
+  #  Bosse = #<Student:0x007fe2ca020233> => name: Bosse_Python, grade: 8, opt_grade_min: 5, opt_grade_max: 11
   #
   # Makes a list of students as object from class Student
 
@@ -26,40 +29,69 @@ def student_list_maker
   end
   return list_of_all_students
 
+end
+
+def presence(list_of_all_students)
+
+  # Public:
+  #
+  # Values:
+  # list_of_all_students - a list of all the students
+  #
+  # How it works/Example:
+  #
+  # list_of_all_students = [Daniel, Bosse]
+  # presence(list_of_all_students)
+  # "Daniel_Berg"
+  # <presses only enter>
+  # "Närvarande"
+  #
+  # "Bosse_Python"
+  # <presses something more and then enter>
+  # "Frånvarande"
+  # # => [Daniel]
+  #
+  #  Daniel = #<Student:0x007fe2ca020968> => name: Daneil_Berg, grade: 9, opt_grade_min: 6, opt_grade_max: 12
+  #  Bosse = #<Student:0x007fe2ca020233> => name: Bosse_Python, grade: 8, opt_grade_min: 5, opt_grade_max: 11
+  #
+  # in this example Bosse is not presence and he is removed from the return array
+  #
+
+
+  list_of_all_students_presence = []
+   position_in_list = 0
+  counter = 0
+  while counter < list_of_all_students.length
+    counter += 1
+    student_at_current_position = list_of_all_students[0 + position_in_list]
+    puts ""
+    puts student_at_current_position.name
+    key_press = gets.chomp
+    if key_press == ""
+      puts "Närvarande"
+      position_in_list += 1
+      list_of_all_students_presence<<(student_at_current_position)
+    else
+      puts "Frånvarande"
+    end
   end
+  return list_of_all_students_presence
+end
+
 
 def main
 
   list_of_all_students = student_list_maker
+  list_of_all_students_presence = presence(list_of_all_students)
 
 
 
-  number = 0
-  lista = 0
-  while lista < list_of_all_students.length
-    lista += 1
-    ropad = list_of_all_students[0 + number]
-     #elever_list[0 + number]
-    puts ""
-    puts ropad.name
 
-    y = gets.chomp
-    if y == ""
-      puts "Närvarande"
-      number += 1
-      list_of_all_missing_students.delete(ropad)
-    else
-      puts "Frånvarande"
-      list_of_all_students.delete(ropad)
-    end
-
-  end
-
-  while list_of_all_students.count >= 2
+  while list_of_all_students_presence.count >= 2
     good_pair = false
-    pair1 = list_of_all_students.sample
-    list_of_all_students.delete(pair1)
-    partner_list = list_of_all_students.dup
+    pair1 = list_of_all_students_presence.sample
+    list_of_all_students_presence.delete(pair1)
+    partner_list = list_of_all_students_presence.dup
     desperate_level = 0
     begin
       file = File.open("#{pair1.name}.txt", "r")
@@ -97,7 +129,7 @@ def main
       if good_pair == false
         partner_list.delete(pair2)
         if partner_list.count == 0
-          partner_list = list_of_all_students.dup
+          partner_list = list_of_all_students_presence.dup
           desperate_level += 1
         end
       end
@@ -123,14 +155,14 @@ def main
     end
 
 
-    list_of_all_students.delete(pair2)
+    list_of_all_students_presence.delete(pair2)
     "#{pair1.name} + #{pair2.name} ".each_char {|c| putc c ; sleep 0.07; $stdout.flush }
     puts ""
     puts "Desperate_level = #{desperate_level}"
     puts ""
   end
-  if list_of_all_students.count == 1
-    pair1 = list_of_all_students.sample
+  if list_of_all_students_presence.count == 1
+    pair1 = list_of_all_students_presence.sample
     "#{pair1.name}".each_char {|c| putc c ; sleep 0.07; $stdout.flush }
   end
   puts ""
